270bfedbe9e65ce79b0b845f8e70daa7a31ab3db
zhanfan9812 <869791827@qq.com>
2020-12-30 11:13:05 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/zhanfan/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhanfan.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
